e575f72407fc7fe19e861d66897e2f76b05d4075
JJongW <shinjw4675@gmail.com>
2024-12-09 16:00:35 +0900

    fix: 환경변수 ㅕㄱ

--- project/src/utils/apiClient.ts
+++ project/src/utils/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001', // 환경 변수로 관리
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://3.141.4.145:3001', // 환경 변수로 관리
   headers: {
     'Content-Type': 'application/json',
   },
