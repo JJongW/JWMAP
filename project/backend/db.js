c0606090b6b5d5e214795a9ea66e8b36267150f1
JJongW <shinjw4675@gmail.com>
2024-12-09 14:49:52 +0900

    fix: DB 수정

--- project/backend/db.js
+++ project/backend/db.js
@@ -3,8 +3,8 @@ require('dotenv').config(); // .env 파일에서 환경 변수 로드
 
 // MySQL 연결 풀 생성
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',       // MySQL 호스트
-  user: process.env.DB_USER || 'root',           // MySQL 사용자 이름
+  host: process.env.DB_HOST || '3.141.4.145',       // MySQL 호스트
+  user: process.env.DB_USER || 'jw',           // MySQL 사용자 이름
   password: process.env.DB_PASSWORD || '', // MySQL 비밀번호
   database: process.env.DB_NAME || 'locations_db', // 데이터베이스 이름
   waitForConnections: true,
