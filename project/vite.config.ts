12087242a2926c8f8194c257ea48c479c0c553c2
JJongW <shinjw4675@gmail.com>
2024-12-09 12:52:48 +0900

    fix: 동적 업로드 변경

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import VitePluginHtmlEnv from 'vite-plugin-html-env'
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePluginHtmlEnv(),
-    VitePluginHtmlEnv({compiler: true})
   ],
   optimizeDeps: {
     exclude: ['lucide-react'],
