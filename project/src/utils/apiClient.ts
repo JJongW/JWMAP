25c89cca6613d45dce9abc6be9af5f80d11b86eb
JJongW <shinjw4675@gmail.com>
2024-12-11 00:21:06 +0900

    fix: 네트워크 에러 수정

--- project/src/utils/apiClient.ts
+++ project/src/utils/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://jwmap.site:3001', // 환경 변수로 관리
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://jwmap.site', // 환경 변수로 관리
   headers: {
     'Content-Type': 'application/json',
   },
