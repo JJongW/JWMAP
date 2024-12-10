8987466b5753d43cd6f3c3efdfab2ab3e0679628
JJongW <shinjw4675@gmail.com>
2024-12-10 19:01:15 +0900

    fix: ip변경

--- project/src/utils/apiClient.ts
+++ project/src/utils/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://3.141.4.145:3001', // 환경 변수로 관리
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://3.130.4.92:3001', // 환경 변수로 관리
   headers: {
     'Content-Type': 'application/json',
   },
