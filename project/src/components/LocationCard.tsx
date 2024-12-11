841abd686db73a1adaa91e966096becbecb4a975
JJongW <shinjw4675@gmail.com>
2024-12-11 23:50:03 +0900

    fix: 오타 수정

--- project/src/components/LocationCard.tsx
+++ project/src/components/LocationCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, memo } from 'react';
+import React, { useState, useEffect } from 'react';
 import type { Location } from '../types/location';
 import { Star, MapPin, Edit2, Trash2 } from 'lucide-react';
 import apiClient from '../utils/apiClient'; 
